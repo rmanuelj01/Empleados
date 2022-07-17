fe7597dbd70806ed7b695480b515923da1e074b6
rmanuelj01 <rmanuelj@uninorte.edu.co>
2022-07-17 16:27:54 -0500

    Modelo empleados añadido y conexión con la DB

--- empleado/models.py
+++ empleado/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class empleado(models.Model):
+    id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50, verbose_name='Nombre')
+    cedula = models.CharField(max_length=50, verbose_name='Cédula')
+    fecha_nacimiento = models.DateField(verbose_name='Fecha de nacimiento')
+    email = models.EmailField(max_length=50)
+    telefono = models.CharField(max_length=50, verbose_name='Teléfono')
+    
