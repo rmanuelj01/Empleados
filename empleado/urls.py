0dcee60bdde0700dbd74bef42d9a64292b525cfe
rmanuelj01 <rmanuelj@uninorte.edu.co>
2022-07-17 15:55:51 -0500

    Agregando la vista de login

--- empleado/urls.py
+++ empleado/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import logout_then_login, LoginView
 
 urlpatterns = [
-    path('', views.inicio, name='inicio'),
+    #path('', views.login, name='login'),
+    path('inicio/', views.inicio, name='inicio'),
+    path('', LoginView.as_view(template_name = 'login.html'), name='login'),
 ]
--- empleado/views.py
+++ empleado/views.py
@@ -4,3 +4,12 @@ from django.shortcuts import render
 
 def inicio(request):
     return render(request, 'index.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+def crear(request):
+    return render(request, 'empleados/crear.html')
+
+def editar(request):
+    return render(request, 'empleados/editar.html')
