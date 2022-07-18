1649b4303ebed2b5d34c1bba98f7f3e1f0baf51d
rmanuelj01 <rmanuelj@uninorte.edu.co>
2022-07-17 23:14:49 -0500

    Funcionalidad de crear un nuevo empleado

--- empleado/urls.py
+++ empleado/urls.py
@@ -3,7 +3,9 @@ from . import views
 from django.contrib.auth.views import logout_then_login, LoginView
 
 urlpatterns = [
-
-    path('inicio/', views.inicio, name='inicio'),
+    
     path('', LoginView.as_view(template_name = 'login.html'), name='login'),
+    path('inicio/', views.inicio, name='inicio'),
+    path('crear/', views.crear, name='crear'),
+    
 ]
--- empleado/views.py
+++ empleado/views.py
@@ -1,18 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from empleado.forms import LibroForm
 from .models import Empleado
 
 # Create your views here.
 
+def login(request):
+    return render(request, 'login.html')
+
 def inicio(request):
     empleados = Empleado.objects.all()
     return render(request, 'empleados/index.html', {'empleados': empleados})
 
-def login(request):
-    return render(request, 'login.html')
-
 def crear(request):
-    return render(request, 'empleados/crear.html')
+    formulario = LibroForm(request.POST or None)
+    if formulario.is_valid():
+        formulario.save()
+        return redirect('inicio')
+    return render(request, 'empleados/crear.html', {'formulario': formulario})
 
 def editar(request):
     return render(request, 'empleados/editar.html')
