e9dee3ae468ef35ae9c729594d5b5ea143b5139e
rmanuelj01 <rmanuelj@uninorte.edu.co>
2022-07-19 17:07:15 -0500

    fix error en el deploy

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^t%w-j!==^gt^p)wh))9ss6&40ztcc$@y)!7sfj(xmev*p23)t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['empleado.herokuapp.com']
 
